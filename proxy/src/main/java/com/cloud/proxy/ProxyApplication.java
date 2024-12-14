35453b98c0a0faf920b617934cc0fa681b776c3f
Christian Gelbolingo <christian.gelbolingo@gmail.com>
2024-12-15 07:27:51 +0800

    Spring Cloud Gateway Initial Commit

--- proxy/src/main/java/com/cloud/proxy/ProxyApplication.java
+++ proxy/src/main/java/com/cloud/proxy/ProxyApplication.java
@@ -3,11 +3,24 @@ package com.cloud.proxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
-public class ProxyApplication {
+public class ProxyApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
+	}
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://httpbin.org:80"))
+				.build();
 	}
 
 }
