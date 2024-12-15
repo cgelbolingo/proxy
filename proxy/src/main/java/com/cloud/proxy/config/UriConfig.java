fd9f1e7b981404b6ed61708d621aedeb4f49f095
Christian Gelbolingo <christian.gelbolingo@gmail.com>
2024-12-16 04:52:13 +0800

    Uncommented testing line

--- proxy/src/main/java/com/cloud/proxy/config/UriConfig.java
+++ proxy/src/main/java/com/cloud/proxy/config/UriConfig.java
@@ -6,10 +6,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties
 class UriConfiguration {
     //Comment for local testing
-//    private String backend = "https://gateway-poc-axh2wfbs7q-ew.a.run.app";
+    private String backend = "https://gateway-poc-axh2wfbs7q-ew.a.run.app";
 
     //Uncomment for local testing
-    private String backend = "http://httpbin.org:80";
+//    private String backend = "http://httpbin.org:80";
 
     public String getBackend() {
         return backend;
